a7dd0b6d0eb5c2c7a299efd4e4c0bb3d69799dac
Serhat Orscek <serhatorscek@gmail.com>
2020-03-15 23:36:48 -0400

    our fifth commit

--- src/gitPackage/GitDayTwo.java
+++ src/gitPackage/GitDayTwo.java
@@ -5,6 +5,14 @@ public class GitDayTwo {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Hello Git Users!");
+		
+		
+		
+		
+		
+	
+	
 	}
 
 }
