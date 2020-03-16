cd388ea23a849b62df77c20b389e674f2d484884
Serhat Orscek <serhatorscek@gmail.com>
2020-03-15 23:42:55 -0400

    commit 6 added

--- src/gitPackage/GitDayTwo.java
+++ src/gitPackage/GitDayTwo.java
@@ -6,7 +6,7 @@ public class GitDayTwo {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello Git Users!");
-		
+		System.out.println("Check if your local is ahead of origin master");
 		
 		
 		
