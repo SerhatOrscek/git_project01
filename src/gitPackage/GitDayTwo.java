3972ffd2b5c8c6a3864d3449a5c36c25938933c0
Serhat Orscek <serhatorscek@gmail.com>
2020-03-15 23:29:47 -0400

    GitDayTwo added

--- src/gitPackage/GitDayTwo.java
+++ src/gitPackage/GitDayTwo.java
@@ -2,9 +2,9 @@ package gitPackage;
 
 public class GitDayTwo {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!");
 	}
 
 }
